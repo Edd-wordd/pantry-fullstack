ddf31a49976f0457c6cc1e0a4508a9bcbafadd0b
Edd-wordd <edward.plasencio@gmail.com>
2020-07-19 17:15:10 -0700

    connect server

--- server.js
+++ server.js
@@ -10,12 +10,21 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query("SELECT 1 + 1 AS solution", (error, results, fields) => {
-   if (error) {
-      console.log(error);
-   } else {
-      console.log("The solution is: ", results[0].solution);
+connection.query(
+   `
+   SELECT 
+      *
+   FROM
+      recipes
+   WHERE
+      recipes.created_by_user_id = '8c3ee2c4-d34d-4fe1-a438-e9ea9271c910'`,
+   (error, results, fields) => {
+      if (error) {
+         console.log(error);
+      } else {
+         console.log(results);
+      }
    }
-});
+);
 
 connection.end();
